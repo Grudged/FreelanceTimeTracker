9b333e30d386562631b9e2cdafa7e01fff98fa4a
grudged <Chrismoore044@gmail.com>
2025-07-23 23:32:55 -0700

    added folder structure

---
+++ src/controllers/controllers.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-controllers',
+  imports: [],
+  templateUrl: './controllers.html',
+  styleUrl: './controllers.css'
+})
+export class Controllers {
+
+}
---
+++ src/middleware/middleware.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-middleware',
+  imports: [],
+  templateUrl: './middleware.html',
+  styleUrl: './middleware.css'
+})
+export class Middleware {
+
+}
